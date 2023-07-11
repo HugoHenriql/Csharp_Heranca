10709e3db692f65873426cd7adfbfa50e6dc53e8
Hugo Henrique de Lara <80010372+HugoHenriql@users.noreply.github.com>
2023-07-11 10:28:32 -0300

    update program e criado SavingsAccount(Up&&Down)

---
+++ CsharpHeranca/Course/Entities/SavingsAccount.cs
@@ -0,0 +1,19 @@
+namespace Course.Entities
+{
+    class SavingsAccount : Account
+    {
+       public double interestRate { get; set; }
+
+       public SavingsAccount()
+       {
+            public SavingsAccount(int number, string holder, double balance, double interestRate) : base(Number, Holder, Balance)
+            {
+                InterestRate = interestRate;
+            }
+
+            public void UpdateBalance()
+            {
+                Balance += Balance * InterestRate;
+            }
+       }
+    }
--- CsharpHeranca/Course/Program.cs
+++ CsharpHeranca/Course/Program.cs
@@ -7,11 +7,35 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc =new BusinessAccount (8010, "Maria", 100.0, 500.0);
+
             BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
 
             Console.WriteLine(account.Balance);
 
-            //account.Balance = 200.0; // error
-        }
+            //UPCASTING
+             
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount (8010, "bob", 100.0, 500.0);
+            Account acc3 = new SavingsAccount (8010, "Anna", 100.0, 500.0); 
+
+            //DOWNCASTING   
+
+            BusinessAccount acc4 = (BusinessAccount) acc2;
+            acc4.Loan(100.0);
+
+            if(acc3 is BusinessAccount){
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+            if(acc3 is SavingsAccount){
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpadateBalance();
+                Console.WriteLine("Update Balance !");
+
+            }        
+}
     }
 }
